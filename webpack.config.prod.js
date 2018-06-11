af2bda3e0cb74239b7b0cd83a637617f23a809f5
Dmitry Kryaklin <dkryaklin@gmail.com>
2018-06-11 12:23:08 +0700

    add build task

---
+++ webpack.config.prod.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/dropdown/index.jsx',
+  mode: 'production',
+  output: {
+    filename: 'dropdown.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js|jsx/,
+        include: path.resolve(__dirname, 'src'),
+        use: 'babel-loader',
+      }, {
+        test: /\.styl$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'stylus-loader',
+        ],
+      },
+    ],
+  },
+};
